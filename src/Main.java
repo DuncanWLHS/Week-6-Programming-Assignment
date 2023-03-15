f14ea06c411171edae7f65c323eed275170771a6
Duncan Duffield <duffied1@wlhs.wlwv.k12.or.us>
2023-03-15 08:51:13 -0700

    Edited Main file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        StatsCalculator testValues = new StatsCalculator(new double[]{2, 1, 3});
-        System.out.println(testValues.calculateSum());
-        testValues.printSorted();
+        StatsCalculator testValues = new StatsCalculator(new double[]{2, 1, 3, 4});
+        System.out.println("The Sum is :" + testValues.calculateSum());
         testValues.print();
+        testValues.printSorted();
         testValues.printFiveNumberSummary();
-        System.out.println(testValues.calculateMean());
+        System.out.println("The Mean is: " + testValues.calculateMean());
     }
 }
